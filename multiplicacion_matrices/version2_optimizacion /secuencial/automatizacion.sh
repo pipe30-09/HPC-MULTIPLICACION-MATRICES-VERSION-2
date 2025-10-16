1b5237929fcc3e0f47a49292c2e0bf32659a9b90
Cespedes-sys <juan.cespedes@utp.edu.co>
2025-10-15 22:44:28 -0500

    Create automatizacion.sh

---
+++ multiplicacion_matrices/version2_optimizacion /secuencial/automatizacion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Para entrar al archivo: cd TIEMPO_AUTOMATIZACION
+# Para dar permisos: chmod +x automatizacion.sh
+# Para ejecutar: ./automatizacion.sh
+
+# Versión 2 (Optimizada con Tiling)
+gcc matriz.c ../../matriz_utils/matriz_utils.c -o matriz_sec_opt # NUEVO: Secuencial Optimizado
